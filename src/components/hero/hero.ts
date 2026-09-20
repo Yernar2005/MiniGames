4a34585a6fa67253d2f378f1f630a6edd1577d2c
Yernar Temirkhan <temirkhanyenrar@gmail.com>
2026-09-20 11:35:19 +0500

    feat: implement responsive hero section

---
+++ src/components/hero/hero.ts
@@ -0,0 +1,15 @@
+import { Component } from "../base-component";
+import heroTemplate from "./hero.html?raw"
+export class Hero extends Component{
+
+    constructor() {
+        super('section', 'hero')
+        this.renderContent()
+    }
+
+
+    private renderContent(): void{
+        this.element.innerHTML = heroTemplate
+
+    }
+}
