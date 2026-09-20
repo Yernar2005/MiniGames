a2c44bcc2147dd3d822cc82f8d0b573138c264f5
Yernar Temirkhan <temirkhanyenrar@gmail.com>
2026-09-20 20:03:06 +0500

    feat: implement game developers section layout

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -2,16 +2,19 @@ import { Hero } from '@/components/hero/hero';
 import { Component } from '../components/base-component';
 import { Carrusel } from '@/components/carrusel/carrusel';
 import { TopPlayers } from '@/components/table/table';
+import { DeveloperCta } from '@/components/dev/dev';
 
 export class HomePage extends Component {
   private hero: Hero;
   private carrusel: Carrusel;
   private topPlayer: TopPlayers;
+  private developerCta: DeveloperCta;
   constructor() {
     super('main', 'home-page');
     this.hero = new Hero();
     this.carrusel = new Carrusel();
     this.topPlayer = new TopPlayers();
+    this.developerCta = new DeveloperCta()
     this.init();
   }
 
@@ -21,5 +24,6 @@ export class HomePage extends Component {
     this.element.appendChild(this.hero.render());
     this.element.appendChild(this.carrusel.render());
     this.element.appendChild(this.topPlayer.render());
+    this.element.appendChild(this.developerCta.render())
   }
 }
